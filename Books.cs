3d0aa499962b7d1f3e78dbf13ad90ed27f5df9ba
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-09 20:56:14 -0500

    task done

---
+++ Books.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsPractice
+{
+    class Books
+    {
+
+        public string title;
+        public string author;
+        public int pages;
+
+        public Books()
+        {
+
+        }
+
+        public Books(string aAuthor, string aTitle, int aPages)
+        {
+           
+            author = aAuthor;
+            title = aTitle;
+            pages = aPages;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConstructorsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books book1 = new Books("Koko","Love Me", 389);
+           // book1.author = "Koko";
+           // book1.title = "Love Me";
+            //book1.pages = 389;
+
+
+            Books book2 = new Books("Tony","Two Things for Certain", 1954);
+            // book2.author = "Tony";
+            //book2.title = "Two Things For Certain";  Constructor replaceds all this work, cleans up code.
+            //book2.pages = 1954;
+
+            Books book3 = new Books();
+
+            book2.pages = 3389;
+            Console.WriteLine(book2.pages);
+
+            
+        }
+    }
+}
